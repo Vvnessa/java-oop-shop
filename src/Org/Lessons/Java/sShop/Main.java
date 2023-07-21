76ded4712b94b834d7030fb520c42f7e8acbb150
Sandy camargo <sandyvanessa95@outlook.com>
2023-07-21 17:13:48 +0200

    prima parte finita

---
+++ src/Org/Lessons/Java/sShop/Main.java
@@ -0,0 +1,19 @@
+package Org.Lessons.Java.sShop;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+Prodotto orologio= new Prodotto(22222222,"orologio","new",400,22);
+System.out.println("orologio");
+
+
+
+
+
+    }
+}
---
+++ src/Org/Lessons/Java/sShop/Prodotto.java
@@ -0,0 +1,88 @@
+package Org.Lessons.Java.sShop;
+import java.util.Random;
+import java.util.UUID;
+public class Prodotto {
+
+
+   private  String nome;
+    private String descrizione;
+     private  double prezzo;
+     private double iva;
+    private int codice;
+
+//costruttore
+
+
+
+    public Prodotto(int codice, String nome, String descrizione, double prezzo, double iva) {
+        this.nome=nome;
+        this.descrizione=descrizione;
+        this.prezzo=prezzo;
+
+                this.iva=iva;
+                this.codice= generateRandomCode();
+
+    }
+
+    //variabile per il numero random
+private int generateRandomCode(){
+    Random randon= new Random();
+    //inseriamo qst xk rrstiuisca un valore
+
+    return  randon.nextInt(100000)+1;
+}
+    //getter and setter
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+//metodi
+
+    public  double Calcolaprezzobase(){
+        return prezzo;
+    }
+    public  double Calcolaprezzoconiva() {
+
+        return prezzo + (prezzo * iva / 100);
+    }
+
+      public  String  GetName(){
+
+        String nomecode =String.format("&08d",codice);
+        return generateRandomCode()+ "-"+ nome;
+    }
+}
+//
